e0400c2d36e0cb6180c127b8178aca8f32259925
LukeMcCann <developer-lukemccann@outlook.com>
2022-01-20 21:51:52 +0000

    Basic Component

---
+++ client/src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem/ExpenseItem.component';
+
+const App = () => {
+    return (
+        <ExpenseItem />
+    );
+};
+
+export default App;
