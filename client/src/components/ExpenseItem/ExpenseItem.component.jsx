3ef81fdd3150ec38223b69828980d192d7c0e065
LukeMcCann <developer-lukemccann@outlook.com>
2022-01-21 21:11:25 +0000

    extracting logic to constants, better practice

--- client/src/components/ExpenseItem/ExpenseItem.component.jsx
+++ client/src/components/ExpenseItem/ExpenseItem.component.jsx
@@ -6,9 +6,19 @@ const ExpenseItem = ({
     date, 
     amount
 }) => {
+
+    const month = date.toLocaleString('en-GB', { month: 'long' } );
+    const day = date.toLocaleString('en-GB', { day: '2-digit' });
+    const year = date.getFullYear();
+
+
     return (
         <div className="expense-item">
-            <div>{date.toISOString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className="expense-item__description">
                 <h2>{title}</h2>
                 <div className="expense-item__price">£{amount}</div>
