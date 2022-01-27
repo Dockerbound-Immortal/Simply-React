20f03da8b884d0e793e3defaa6918af168ad6121
LukeMcCann <developer-lukemccann@outlook.com>
2022-01-27 22:18:28 +0000

    Update state example

--- client/src/components/Expenses/ExpenseItem/ExpenseItem.component.jsx
+++ client/src/components/Expenses/ExpenseItem/ExpenseItem.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate/ExpenseDate.component';
 import PriceTag from './PriceTag/PriceTag.component';
 import Card from '../../Card/Card.component';
@@ -8,16 +8,28 @@ const ExpenseItem = ({
     title, 
     date, 
     amount
-}) => (
-    <Card 
-        classes={`expense-item`}
-    >
-        <ExpenseDate date={date} />
-        <div className='expense-item__description'>
-            <h2>{title}</h2>
-            <PriceTag amount={amount} />
-        </div>
-    </Card>
-);
+}) => {
+
+    const [rubric, setRubric] = useState(title);
+
+    const handleOnClick = () => {
+        setRubric('Updated');
+    };
+
+    return (
+        <Card 
+            classes={`expense-item`}
+        >
+            <ExpenseDate date={date} />
+            <div className='expense-item__description'>
+                <h2>{rubric}</h2>
+                <PriceTag amount={amount} />
+            </div>
+            <button
+                onClick={handleOnClick }
+            >Change Title</button>
+        </Card>
+    );
+};
 
 export default ExpenseItem;
