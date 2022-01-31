1b9f19321c39293fac17f9af0a1de2f60517b7ea
LukeMcCann <developer-lukemccann@outlook.com>
2022-01-31 19:30:48 +0000

    Initial commit

--- client/src/components/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import expenses from '../mockdata/expenses';
-import Expenses from './Expenses/Expenses.component';
-
-const App = () => (
-    <Expenses expenses={expenses} />
-);
-
-export default App;
